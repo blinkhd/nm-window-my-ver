069fc9d6075bc110458abc57bbec9c0a2ca9fc77
vega79 <95876495+vega79@users.noreply.github.com>
2022-04-27 09:47:01 -0500

    Update tabcloaktmp.js

--- tabcloaktmp.js
+++ tabcloaktmp.js
@@ -1,26 +1,20 @@
 var title = document.createElement("div")
 title.className = "tabtitle"
-title.innerText = "Tab Cloak"
+title.innerText = "History Flooder"
 appid.appendChild(title)
 
 var vbuttons = document.createElement("center")
 
 var vtab = document.createElement("input")
 vtab.id = "tab"
-vtab.placeholder = "Enter title or icon"
+vtab.placeholder = "How many times"
 vbuttons.appendChild(vtab)
 
 var vbutton1 = document.createElement("button")
 vbutton1.className = "settitle"
-vbutton1.setAttribute("onclick", "settitle()")
-vbutton1.innerText = "Title"
+vbutton1.setAttribute("onclick", "floodhistory()")
+vbutton1.innerText = "OK"
 vbuttons.appendChild(vbutton1)
-
-var vbutton2 = document.createElement("button")
-vbutton2.className = "seticon"
-vbutton2.setAttribute("onclick", "seticon()")
-vbutton2.innerText = "Icon"
-vbuttons.appendChild(vbutton2)
 
 appid.appendChild(vbuttons)
 
@@ -30,61 +24,14 @@ document.getElementsByTagName("html")[0].appendChild(vaporstyle)
 
 var vaporscript = document.createElement("script")
 vaporscript.innerHTML = `
-var oldtitle = document.title
-var oldfavicontype = "none"
-var oldfavicon;
-
-var favicon1 = document.querySelector("link[rel='icon']")
-var favicon2 = document.querySelector("link[rel='shortcut icon']")
-
-if (favicon1) {
-oldfavicontype = "favicon1"
-oldfavicon = favicon1.href
-} else if (favicon2) {
-oldfavicontype = "favicon2"
-oldfavicon = favicon2.href
-}
-
-function settitle() {
+function floodhistory() {
 var tab = document.getElementById("tab")
-if (tab.value !== "") {
-document.title = tab.value
-} else {
-document.title = oldtitle
-}
+var num=tab.value;
+done=false;x=window.location.href;
+for (var i=1; i<=num; i++){history.pushState(0, 0, i==num?x:i.toString());
+if(i==num){done=true}}if(done===true){}
 tab.value = ""
 }
-
-function seticon() {
-var tab = document.getElementById("tab")
-var favicon1 = document.querySelector("link[rel='icon']")
-var favicon2 = document.querySelector("link[rel='shortcut icon']")
-
-if (tab.value !== "") {
-if (favicon1) {
-favicon1.href = tab.value
-} else if (favicon2) {
-favicon2.href = tab.value
-} else {
-var favicon = document.createElement("link")
-favicon.rel = "icon"
-favicon.href = tab.value
-document.getElementsByTagName("head")[0].appendChild(favicon)
-}
-} else {
-if (oldfavicontype == "favicon1") {
-favicon1.href = oldfavicon
-} else if (oldfavicontype == "favicon2") {
-favicon2.href = oldfavicon
-} else {
-if (favicon1) {
-favicon1.remove()
-} else if (favicon2) {
-favicon2.remove()
-}
-}
-}
-tab.value = ""
-}`
+`
 document.getElementsByTagName("html")[0].appendChild(vaporscript)
 addcomment
