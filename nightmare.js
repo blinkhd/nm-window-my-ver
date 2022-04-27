8c6dec618530aa9ec6635595a1438ca21c0fc568
vega79 <95876495+vega79@users.noreply.github.com>
2022-04-27 10:08:09 -0500

    Update nightmare.js

--- nightmare.js
+++ nightmare.js
@@ -1,42 +1,3 @@
-/*var iframeui = new Nightmare({
-    "title": "IFrame",
-    "type": "js",
-    "js": "https://raw.githubusercontent.com/Avad3/bookmarklets/main/app.js",
-    "width": 600,
-    "height": 400,
-    "theme": "dark"
-    })
-    
-    async function injectnightmare() {
-    var NightmareWindows = await fetch("https://cdn.jsdelivr.net/gh/FogNetwork/Nightmare-Windows/windows.min.js")
-    var NightmareCode = await NightmareWindows.text()
-    NightmareCode = NightmareCode.replaceAll("<br>", ";")
-    var cdn1 = document.createElement("script")
-    cdn1.innerHTML = NightmareCode  + "\n\n" + "eval(" + iframeui + ")"
-    document.getElementsByTagName("html")[0].appendChild(cdn1)
-    }
-    
-    var cdn2 = document.createElement("link")
-    cdn2.setAttribute("rel", "stylesheet")
-    cdn2.href = "https://cdn.jsdelivr.net/gh/FogNetwork/Nightmare-Windows/windows.css"
-    document.getElementsByTagName("html")[0].appendChild(cdn2)
-    
-    var ncdn3 = document.createElement("script")
-    ncdn3.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"
-    document.getElementsByTagName("html")[0].appendChild(ncdn3)
-    
-    var ncdn4 = document.createElement("script")
-    ncdn4.src = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"
-    document.getElementsByTagName("html")[0].appendChild(ncdn4)
-    
-    var ncdn5 = document.createElement("link")
-    ncdn5.rel = "stylesheet"
-    ncdn5.href = "https://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css"
-    document.getElementsByTagName("html")[0].appendChild(ncdn5)
-    
-    
-injectnightmare()*/
-
 var cdn1 = document.createElement("script")
 cdn1.src = "https://cdn.jsdelivr.net/gh/FogNetwork/Nightmare-Windows/windows.min.js"
 document.getElementsByTagName("html")[0].appendChild(cdn1)
@@ -64,7 +25,7 @@ new Nightmare({
     "type": "js", //URL, Html or Js - Required
     "url": "https://example.com", //For url
     "html": "<button>hi</button>", //For html
-    "js": "https://cdn.jsdelivr.net/gh/vega79/nightmare/nightmaresrc.min.js", //For js url
+    "js": "https://raw.githubusercontent.com/vega79/nightmare/main/nightmaresrc.js //For js url
     "width": 800, //Width - No quotes
     "height": 600, //Height - No quotes
     "theme": "dark" //Light or Dark
