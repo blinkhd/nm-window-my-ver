ee1439193db38419ca1ccd14dfef9b3dcdb65462
vega79 <95876495+vega79@users.noreply.github.com>
2022-04-27 12:10:54 -0500

    commit

--- nightmare.js
+++ nightmare.js
@@ -25,7 +25,7 @@ new Nightmare({
     "type": "js",
     "url": "",
     "html": "",
-    "js": "https://cdn.jsdelivr.net/gh/vega79/nightmare@0bd539ebdde4107b7f5e176b0c2fac873bb73a7d/nightmaresrc.js",
+    "js": "https://cdn.jsdelivr.net/gh/vega79/nightmare@8b89087/nightmaresrc.js",
     "width": 800,
     "height": 600,
     "theme": "dark",
